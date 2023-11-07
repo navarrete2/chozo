cb4b5c99cb79058fef87d59a9952a6d5aa998b58
navarrete2 <m4gu4l@icloud.com>
2023-11-07 22:34:47 +0100

    subimos archivo uno de xcode

---
+++ chozo/chozo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chozo
+//
+//  Created by Miguel  on 7/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("hola, mundo!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chozo/chozo/chozoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chozoApp.swift
+//  chozo
+//
+//  Created by Miguel  on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct chozoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
